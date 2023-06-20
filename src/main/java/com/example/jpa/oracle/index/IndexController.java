55d408f46f6aef5cd0cf5b8da56ceb55f3a6766f
GIT-SIK <xeno_s@naver.com>
2023-06-20 21:17:37 +0900

    log - A

--- src/main/java/com/example/jpa/oracle/index/IndexController.java
+++ src/main/java/com/example/jpa/oracle/index/IndexController.java
@@ -1,4 +1,18 @@
 package com.example.jpa.oracle.index;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class IndexController {
+    @GetMapping("/log")
+    public String IndexLog(Model model){
+        String sLog = " #### INDEX LOG TEST ####";
+        log.info(sLog);
+        model.addAttribute("logdata",sLog );
+        return "log.html";
+    }
 }
